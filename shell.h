ab7f7a41ff6938b195324f624a382df3fbbdb165
ElaasriTarik <elaasriya2003@gmail.com>
2023-08-15 20:05:17 +0100

    update

--- shell.h
+++ shell.h
@@ -13,6 +13,7 @@ void display_prompt(void);
 int _strlen(char *c);
 int _strcmp(const char *str1, const char *str2);
 char _getPath(char **envp);
+char **split_string(const char *input, char delimiter, size_t *word_count);
 
 
 /**
@@ -54,14 +55,10 @@ int _strcmp(const char *str1, const char *str2)
 
 int _strncmp(const char *s1, const char *s2, size_t n)
 {
-    while (n > 0 && *s1 && (*s1 == *s2))
-    {
-        s1++;
-        s2++;
-        n--;
-    }
+    for (;n > 0 && *s1 && (*s1 == *s2); n--, s1++, s2++)
+        ;
     if (n == 0)
-    return 0;
+    return (0);
 
     return *(unsigned char *)s1 - *(unsigned char *)s2;
 }
@@ -75,6 +72,7 @@ void process_input(const char *user_input, char **envp)
 {
         int i = 0;
         char *path = NULL;
+
         char *goodbye_msg = "Goodbye, exiting shell.\n";
         
         if (_strcmp(user_input, "exit") == 0)
@@ -103,7 +101,7 @@ void process_input(const char *user_input, char **envp)
         else
         write(STDOUT_FILENO, "\nNO PATH DETECTED \n", 20);
         }
-
+        
 }
 
 #endif
