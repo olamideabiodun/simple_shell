082a56c796d3a0cea690361e238947587536b265
Abdessadek EL AASRI <37253593+ElaasriTarik@users.noreply.github.com>
2023-08-23 14:22:41 +0100

    Update handle_builtin.c

--- handle_builtin.c
+++ handle_builtin.c
@@ -13,18 +13,17 @@ void builtIn(char **input, char **envp)
     char *goodbye_msg = "Goodbye, exiting shell.\n";
     if (_strcmp(input[0], "env") == 0 || _strcmp(input[0], "printenv") == 0)
     {
-      for (; envp[i] != NULL; i++)
-	{
-	write(STDOUT_FILENO, envp[i], _strlen(envp[i]));
-	write(STDOUT_FILENO, "\n", 1);
-	}
+        for (; envp[i] != NULL; i++) {
+            write(STDOUT_FILENO, envp[i], _strlen(envp[i]));
+            write(STDOUT_FILENO, "\n", 1);
+        }
     }
     if (_strcmp(input[0], "exit") == 0)
-      {
-	if (input[1] != NULL)
-	  {
-	    status = _atoi(input[1]);
-	    write(STDOUT_FILENO, goodbye_msg, 25);
-	    exit(status);
-	  }
+        {
+            if (input[1] != NULL)
+                status = _atoi(input[1]);
+
+            write(STDOUT_FILENO, goodbye_msg, 25);
+            exit(status);
+        }
 }
