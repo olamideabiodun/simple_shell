6fdd7299a21a1aec7b04f375f07234c567b2d006
Abdessadek EL AASRI <37253593+ElaasriTarik@users.noreply.github.com>
2023-08-24 03:46:25 +0100

    Update handle_builtin.c

--- handle_builtin.c
+++ handle_builtin.c
@@ -6,11 +6,9 @@
  * @input: command
  * @env: env
 */
-void builtIn(char **input, char **envp, char *user_input)
+void builtIn(char **input, char **envp, char *user_input, int *exitStat)
 {
     int i = 0;
-    int status;
-    char *goodbye_msg = "Goodbye, exiting shell.\n";
     if (_strcmp(input[0], "env") == 0 || _strcmp(input[0], "printenv") == 0)
     {
         for (; envp[i] != NULL; i++) {
@@ -20,13 +18,8 @@ void builtIn(char **input, char **envp, char *user_input)
     }
     if (_strcmp(input[0], "exit") == 0)
         {
-            if (input[1] != NULL)
-                status = _atoi(input[1]);
-	    else
-            status = 0;
 	    free(user_input);
 	    free_string(input);
-            write(STDOUT_FILENO, goodbye_msg, 25);
-            exit(status);
+            exit(*exitStat);
         }
 }
